4aee760d7b0475425b54bca6cdb1c7762a67030b
Shred258 <billymillitha03@yahoo.com>
2025-05-16 22:51:55 +0300

    Latest_updat

--- voteproject/voteproject/wsgi.py
+++ voteproject/voteproject/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from django.core.wsgi import get_wsgi_application
 
